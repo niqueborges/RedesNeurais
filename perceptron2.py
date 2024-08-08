3d7b40cc4b007b8a17eb670244e1033960d0873f
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-08 16:31:07 -0300

    feat: add update perceptron2.py

--- perceptron2.py
+++ perceptron2.py
@@ -2,19 +2,14 @@
 
 import numpy as np # Importa a biblioteca numpy
 
-entradas = [1, 7, 5]
-pesos = [0.8, 0.1, 0]
+entradas = np.array([1, 7, 5]) # Define as entradas como um array numpy
+pesos = np.array([0.8, 0.1, 0]) # Define os pesos como um array numpy
 
 def soma(e, p): # Função que calcula a soma ponderada
-    s = 0 # Inicializa a variável s
-    for i in range(3): # Loop para percorrer as entradas e pesos
-        #print(entradas[i]) 
-        #print(pesos[i])
-        s += e[i] * p[i] # Calcula a soma ponderada
-    return s # Retorna o valor da soma ponderada
+    return e.dot(p) # Retorna o valor da soma ponderada
+# dot product / produto escalar
         
 s = soma(entradas, pesos) # Chama a função soma com as entradas e pesos
-
 print(s) # Exibe o resultado   
 
 def stepFunction(soma): 
@@ -23,31 +18,23 @@ def stepFunction(soma):
     return 0
 
 r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada 
-
-print(r)  # Exibe o resultado
+print(r)  
 
 # Exibe o resultado de ativação ou não do neurônio
 
-entradas = [-1, 7, 5]
-pesos = [0.8, 0.1, 0]
+entradas = np.array([-1, 7, 5])
+pesos = np.array([0.8, 0.1, 0])
 
-def soma(e, p): # Função que calcula a soma ponderada
-    s = 0 # Inicializa a variável s
-    for i in range(3): # Loop para percorrer as entradas e pesos
-        #print(entradas[i]) 
-        #print(pesos[i])
-        s += e[i] * p[i] # Calcula a soma ponderada
-    return s # Retorna o valor da soma ponderada
+def soma(e, p): 
+   return e.dot(p) 
         
-s = soma(entradas, pesos) # Chama a função soma com as entradas e pesos
-
-print(s) # Exibe o resultado   
+s = soma(entradas, pesos) 
+print(s) 
 
 def stepFunction(soma): 
     if (soma >= 1):
         return 1
     return 0
 
-r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada 
-
-print(r)  # Exibe o resultado
+r = stepFunction(s) 
+print(r)  
