c12ec2c71f9c1566538b6c9223d679991c098734
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-08 15:46:37 -0300

    feat: update perceptron1

--- perceptron1.py
+++ perceptron1.py
@@ -23,3 +23,29 @@ def stepFunction(soma):
 r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada 
 
 print(r)  # Exibe o resultado
+
+# Exibe o resultado de ativação ou não do neurônio
+
+entradas = [-1, 7, 5]
+pesos = [0.8, 0.1, 0]
+
+def soma(e, p): # Função que calcula a soma ponderada
+    s = 0 # Inicializa a variável s
+    for i in range(3): # Loop para percorrer as entradas e pesos
+        #print(entradas[i]) 
+        #print(pesos[i])
+        s += e[i] * p[i] # Calcula a soma ponderada
+    return s # Retorna o valor da soma ponderada
+        
+s = soma(entradas, pesos) # Chama a função soma com as entradas e pesos
+
+print(s) # Exibe o resultado   
+
+def stepFunction(soma): 
+    if (soma >= 1):
+        return 1
+    return 0
+
+r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada 
+
+print(r)  # Exibe o resultado
