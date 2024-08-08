e9d9a8fc3668247a1c2db3780908c0147a1a7e12
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-08 15:38:50 -0300

    feat: update perceptron1

--- perceptron1.py
+++ perceptron1.py
@@ -1,4 +1,4 @@
-# Description: Perceptron simples com 3 entradas e 3 pesos
+# Description: Perceptron simples (de uma camada) com 3 entradas e 3 pesos
 
 entradas = [1, 7, 5]
 pesos = [0.8, 0.1, 0]
@@ -11,13 +11,15 @@ def soma(e, p): # Função que calcula a soma ponderada
         s += e[i] * p[i] # Calcula a soma ponderada
     return s # Retorna o valor da soma ponderada
         
-s = soma(entradas, pesos) # Chama a função soma com as entradas e pesos   
+s = soma(entradas, pesos) # Chama a função soma com as entradas e pesos
+
+print(s) # Exibe o resultado   
 
 def stepFunction(soma): 
     if (soma >= 1):
         return 1
     return 0
 
-r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada
+r = stepFunction(s) # Chama a função stepFunction com o valor da soma ponderada 
 
-       
+print(r)  # Exibe o resultado
